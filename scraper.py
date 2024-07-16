b996155329eebfcb82c6055a4eccbe4f5ac57dc2
Rafael Jardim de Lima <rafajluci@gmail.com>
2024-07-16 15:21:07 -0300

    função fechar pop-up sem testes

--- scraper.py
+++ scraper.py
@@ -4,6 +4,19 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+def fechar_popup(driver):
+    try:
+        popup = driver.find_elements("xpath", "//div[contains(@class, 'fancybox-overlay fancybox-overlay-fixed')]")
+        if popup:
+            close_button = driver.find_element("xpath", "//button[contains(@class, 'fancybox-item fancybox-close')]")
+            close_button.click()
+            time.sleep(1)
+            print("Pop-up fechada.")
+        else:
+            print("Nenhuma pop-up encontrada.")
+    except Exception as e:
+        print(f"Erro ao tentar fechar a pop-up: {e}")
+
 url = "https://apnews.com/"
 
 options = Options()
@@ -14,13 +27,17 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get(url)
 driver.maximize_window() 
 
+fechar_popup(driver)
 search_button = driver.find_element("xpath", "//button[@class='SearchOverlay-search-button']")
 search_button.click()
 
-time.sleep(1)
+time.sleep(2)
 
+fechar_popup(driver)
 search_input = driver.find_element("xpath", "//label[@class='SearchOverlay-search-label']")
 search_input.send_keys("Olympic Games")
 search_input.send_keys(u'\ue007')
 
-time.sleep(2)
+time.sleep(60)
+
+fechar_popup(driver)
