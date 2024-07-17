c3e8399b96e0b875d79626829b20d75669914c73
Rafael Jardim de Lima <rafajluci@gmail.com>
2024-07-17 02:17:37 -0300

    Refatoração do código para melhor compreensão. Exclusão da função de popup

--- scraper.py
+++ scraper.py
@@ -1,31 +1,22 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-def fechar_popup(driver):
-    try:
-        popup = driver.find_element("xpath", "//div[@class='fancybox-overlay fancybox-overlay-fixed')]")
-        if popup:
-            search_button = driver.find_element("xpath", "//button[@class='fancybox-item fancybox-close')]")
-            search_button.click()
-            time.sleep(1)
-            print("Pop-up fechada.")
-        else:
-            print("Nenhuma pop-up encontrada.")
-    except Exception as e:
-        print(f"Erro ao tentar fechar a pop-up: {e}")
-
+#Definindo URL
 url = "https://apnews.com/"
 
-# Função para pedir um parâmetro ao usuário
+#Definindo a frase
 def definir_frase():
     frase = input("Por favor, informe oque deseja pesquisar: ")
     return frase
 frase = definir_frase()
 
+#Dicionario de categorias
 categorias = {
     "Featured Articles": "00000190-08f3-d7b0-a1fa-d9f37cb90000",
     "Live Blogs": "00000190-0dc5-d7b0-a1fa-dde7ec030000",
@@ -37,58 +28,56 @@ categorias = {
 }
 
 while True:
+    #Exibindo as categorias
+    print("Por favor escolha uma das seguintes categorias disponíveis:")
+    for categoria in categorias:
+        print(categoria)
+    
+    #Escolhendo a categoria
     categoria_escolhida = input("Informe a categoria desejada: ")
     numero_associado = categorias.get(categoria_escolhida)
-
+    
+    #Verificando se a categoria existe
     if numero_associado:
         print(f"Aguarde enquanto a categoria {categoria_escolhida} é carregada.")
         break
     else:
         print("Categoria inválida. Por favor, tente novamente.")
 
+#Configurando Selenium
 options = Options()
 options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#Instanciando o WebDriver
+driver = webdriver.Chrome(options=options)
+wait = WebDriverWait(driver, 10)
 
+#Acessando o site
 driver.get(url)
 driver.maximize_window() 
 
-fechar_popup(driver)
-search_button = driver.find_element("xpath", "//button[@class='SearchOverlay-search-button']")
-search_button.click()
+#Clicando no botão de pesquisa e enviando a frase
+search_button = driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-button').click()
+time.sleep(2)
+search_input = driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-label')
+search_input.send_keys(frase)
+search_input.send_keys(u'\uE007')
 time.sleep(2)
 
-fechar_popup(driver)
-search_input = driver.find_element("xpath", "//label[@class='SearchOverlay-search-label']")
-search_input.send_keys(frase)
-search_input.send_keys(u'\ue007')
-time.sleep(5)
-
+#Scroll para ver melhor os resultados
 def scroll_down():
     driver.execute_script("window.scrollBy(0, 300);")
-
 for _ in range(1):
     scroll_down()
 
-fechar_popup(driver)
-search_button = driver.find_element("xpath", "//bsp-toggler[@class='SearchFilter-content']")
-search_button.click()
+#Abrindo o filtro e selecionando a opção do usuário
+filter_wait = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'SearchFilter-content')))
+filter_toggle = driver.find_element(By.CLASS_NAME, "SearchFilter-content").click()
 time.sleep(1)
+seeall_button = driver.find_element(By.CLASS_NAME, "SearchFilter-seeAll-button").click()
+time.sleep(1)
+checkbox = driver.find_element("xpath", f"//input[@type='checkbox' and @value='{numero_associado}']").click()
 
-fechar_popup(driver)
-search_button = driver.find_element("xpath", "//button[@class='SearchFilter-seeAll-button']")
-search_button.click()
-time.sleep(2)
-
-fechar_popup(driver)
-checkbox = driver.find_element("xpath", f"//input[@type='checkbox' and @value='{numero_associado}']")
-checkbox.click()
-time.sleep(2)
-
-fechar_popup(driver)
+order_wait = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'Select-input')))
 dropdown = Select(driver.find_element("xpath", "//select[@name='s' and contains(@class, 'Select-input')]"))
 dropdown.select_by_visible_text("Newest")
-
-time.sleep(120)
-fechar_popup(driver)
