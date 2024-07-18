0cdc8805da480a215e641f9086593b11900c0d96
Rafael Jardim de Lima <rafajluci@gmail.com>
2024-07-18 11:09:13 -0300

    Reduzindo o for, e verificando a data

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from datetime import datetime
 import re
 import pandas as pd
 
@@ -40,17 +41,18 @@ driver.get(url)
 driver.maximize_window()
 
 #Clicando no botão de pesquisa e enviando a frase
-search_button = driver.find_element(By.XPATH, "/html/body/ps-header/header/div[2]/button").click()
+botao_pesquisa = driver.find_element(By.XPATH, "/html/body/ps-header/header/div[2]/button").click()
 time.sleep(2)
-search_input = driver.find_element(By.XPATH, "/html/body/ps-header/header/div[2]/div[2]/form/label/input")
-search_input.send_keys(frase)
-search_input.send_keys(u'\uE007')
+input_pesquisa = driver.find_element(By.XPATH, "/html/body/ps-header/header/div[2]/div[2]/form/label/input")
+input_pesquisa.send_keys(frase)
+input_pesquisa.send_keys(u'\uE007')
 time.sleep(2)
 
-seeall_topics = driver.find_element("xpath", "/html/body/div[2]/ps-search-results-module/form/div[2]/ps-search-filters/div/aside/div/div[3]/div[1]/ps-toggler/ps-toggler/button").click()
+#Expandindo os topicos
+seeall_topico = driver.find_element("xpath", "/html/body/div[2]/ps-search-results-module/form/div[2]/ps-search-filters/div/aside/div/div[3]/div[1]/ps-toggler/ps-toggler/button").click()
 
-#Abrindo o filtro e selecionando a opção do usuário
-filter_wait = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'checkbox-input-element')))
+#Procurando o topico e selecionando a opção do usuário
+topico_wait = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'checkbox-input-element')))
 listatopicos = driver.find_element(By.XPATH, '/html/body/div[2]/ps-search-results-module/form/div[2]/ps-search-filters/div/aside/div/div[3]/div[1]/ps-toggler/ps-toggler/div/ul')
 filhos = listatopicos.find_elements(By.TAG_NAME, "li")
 
@@ -67,15 +69,17 @@ for filho in filhos:
 if (encontroutopico == 0):
     print("nenhum topico foi encontrado")
     exit()
-time.sleep(5)
+time.sleep(8)
 
+#Selecionando noticias mais recentes
 dropdown = Select(driver.find_element("xpath", "//select[@name='s' and contains(@class, 'select-input')]"))
 dropdown.select_by_visible_text("Newest")
-time.sleep(5)
+time.sleep(8)
 
-seeall_type = driver.find_element(By.XPATH, "/html/body/div[2]/ps-search-results-module/form/div[2]/ps-search-filters/div/aside/div/div[3]/div[2]/ps-toggler/ps-toggler/button").click()
-time.sleep(5)
+#Expandindo os tipos
+seeall_tipo = driver.find_element(By.XPATH, "/html/body/div[2]/ps-search-results-module/form/div[2]/ps-search-filters/div/aside/div/div[3]/div[2]/ps-toggler/ps-toggler/button").click()
 
+#Procurando o tipo e selecionando a opção do usuário
 listatipo = driver.find_element(By.XPATH, '/html/body/div[2]/ps-search-results-module/form/div[2]/ps-search-filters/div/aside/div/div[3]/div[2]/ps-toggler/ps-toggler/div/ul')
 filhos = listatipo.find_elements(By.TAG_NAME, "li")
 
@@ -92,38 +96,50 @@ for filho in filhos:
 if (encontroutipo == 0):
     print("nenhum tipo foi encontrado")
     exit()
-time.sleep(5)
+time.sleep(8)
 
+#Definindo o ul e li que recebem as notícias
 listanoticias = driver.find_element(By.XPATH, '/html/body/div[2]/ps-search-results-module/form/div[2]/ps-search-filters/div/main/ul')
 filhos = listanoticias.find_elements(By.TAG_NAME, "li")
 
+#Definindo a data atual
+data_atual = datetime.now()
+mes_anterior = data_atual.month - 1 if data_atual.month > 1 else 12
+ano_mes_anterior = data_atual.year if data_atual.month > 1 else data_atual.year - 1
+
+#Abrindo listas para receber os dados
 tituloslista = []
-for filho in filhos:
-    h3filho = filho.find_element(By.TAG_NAME, "h3")
-    ahreffilho = h3filho.find_element(By.TAG_NAME, "a")
-    tituloslista.append(ahreffilho.text)
-
 dataslista = []
-for filho in filhos:
-    datafilho = filho.find_element(By.CLASS_NAME, 'promo-timestamp')
-    dataslista.append(datafilho.text)
-
 descricoeslista = []
 valor_monetariolista = []
+
+#Repetição para capturar as informaçoes das notícias
 for filho in filhos:
-    try:
-        descricaofilho = filho.find_element(By.CLASS_NAME, 'promo-description').text
-    except:
-        descricaofilho = None
-    descricoeslista.append(descricaofilho)
+    datafilho = filho.find_element(By.CLASS_NAME, 'promo-timestamp').text
+    datafilho_datetime = datetime.strptime(datafilho, '%B %d, %Y')
+    #Condição para verificar se a data é do mês atual ou mês passado
+    if(datafilho_datetime.year == data_atual.year and datafilho_datetime.month == data_atual.month) or (datafilho_datetime.year == ano_mes_anterior and datafilho_datetime.month == mes_anterior):
+        #Inserindo dados nas listas
+        h3filho = filho.find_element(By.TAG_NAME, "h3")
+        ahreffilho = h3filho.find_element(By.TAG_NAME, "a")
+        tituloslista.append(ahreffilho.text)
+        dataslista.append(datafilho)
 
-    valor_monetario = re.search(r'(\$\d+[\,*\d+]*\.*\d*|\d+\sdollars|\d+\sUSD)', descricaofilho)
-    
-    if valor_monetario:
-        valor_monetariolista.append(valor_monetario.group())
-    else:
-        valor_monetariolista.append(None)
+        #Inserindo descrição caso tenha
+        try:
+            descricaofilho = filho.find_element(By.CLASS_NAME, 'promo-description').text
+        except:
+            descricaofilho = None
+        descricoeslista.append(descricaofilho)
 
-planilha = pd.DataFrame({'titulo': tituloslista, 'data': dataslista, 'descrição': descricoeslista, 'valor_monetário': valor_monetariolista})
+        #Inserindo valor monetário caso tenha
+        valor_monetario = re.search(r'(\$\d+[\,*\d+]*\.*\d*|\d+\sdollars|\d+\sUSD)', descricaofilho)
+        if valor_monetario:
+            valor_monetariolista.append(valor_monetario.group())
+        else:
+            valor_monetariolista.append(None)
+
+#Insertando dados na planilha
+planilha = pd.DataFrame({'Titulo': tituloslista, 'Data': dataslista, 'Descrição': descricoeslista, 'Valor_monetário': valor_monetariolista})
 planilha.to_excel('planilha.xlsx', index=False)
 planilha.to_csv('planilha.csv', index=False)
